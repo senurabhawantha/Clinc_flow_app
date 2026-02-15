998afcb60a7b4f21d5f2459a1bc3665b2987a12b
Senura Bhawantha <senurabawantha@gmail.com>
2026-02-15 12:56:18 +0530

    Initial Commit

---
+++ Clinc_flow_app/Clinc_flow_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clinc_flow_appApp.swift
+//  Clinc_flow_app
+//
+//  Created by ITEDP on 2026-02-15.
+//
+
+import SwiftUI
+
+@main
+struct Clinc_flow_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
