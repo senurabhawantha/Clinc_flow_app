4219858908c87ef21ce40b7b1c50311308d7cd17
Senura Bhawantha <senurabawantha@gmail.com>
2026-02-17 12:46:55 +0530

    Startup

--- Clinc_flow_app/ContentView.swift
+++ Clinc_flow_app/ContentView.swift
@@ -4,21 +4,64 @@
 //
 //  Created by ITEDP on 2026-02-15.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Clinc Flow App")
+        NavigationStack {
+            ZStack {
+                
+                Image("backgroundImage2")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    
+                    Text("Welcome")
+                        .font(.custom("Futura", size: 65))
+                        .foregroundColor(.white)
+                        .padding(.top, 125)
+
+                    Spacer()
+
+                    
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(.blue)
+
+                    Spacer()
+
+                    
+                    VStack(spacing: 20) {
+                        Text("X")
+                            .font(.custom("Futura", size: 17))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 30)
+
+                    
+                        NavigationLink(destination: LoginView()) {
+                            Text("Get Started")
+                                .foregroundColor(.white)
+                                .font(.custom("Futura", size: 25))
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(12)
+                                .padding(.horizontal, 40)
+                        }
+                    }
+                    .padding(.bottom, 75)
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
